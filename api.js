005df784a05d3059d1dab51385bae06899d808d1
*твое <*>
2023-06-26 20:09:25 +0500

    course new

--- api.js
+++ api.js
@@ -1,5 +1,3 @@
-// import { ru } from "date-fns/locale";
-// import { formatDistanceToNow } from "date-fns";
 
 const personalKey = "ramzil-khalimov";
 const baseHost = "https://wedev-api.sky.pro";
@@ -61,7 +59,7 @@ export function addPosts({ description, imageUrl, token }) {
   });
 };
 
-// https://github.com/GlebkaF/webdev-hw-api/blob/main/pages/api/user/README.md#%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D1%8C%D1%81%D1%8F
+
 export function registerUser({ login, password, name, imageUrl }) {
   return fetch(baseHost + "/api/user", {
     method: "POST",
